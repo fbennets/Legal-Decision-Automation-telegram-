36bbc21625ae7b580366ab72abcd0e8d23756fcf
fbennets <bennet_s@protonmail.com>
2020-05-23 19:56:43 +0200

    Removed custom settings

--- settings/custom_settings.py
+++ settings/custom_settings.py
@@ -1,5 +1,5 @@
 OD_URL = ''
 DEMO_QUERY = ''
 LANG = 'en'
-GREET_IMAGE = 'https://cdn.jetphotos.com/full/6/55271_1550157114.jpg'
-QUERY_LIST = [['Rücklastschrift Gebühren', 'habbiovzxt'], ['Fine Contradiction', 'habbiovzxa']]
+GREET_IMAGE = ''
+QUERY_LIST = [['Rücklastschrift Gebühren', 'habbiovzxt']]
